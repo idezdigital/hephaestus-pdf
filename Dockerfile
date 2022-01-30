e9fd237dec6a2b83bcbcea747ab36eb0e6bc64b2
gabrieldebem <gabrieldebem72@gmail.com>
2022-01-30 15:47:00 -0300

    add error handling and update node version to latest

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
-FROM node:14-slim
+FROM node:latest
+
+WORKDIR /usr/src/app
+
+
+COPY package*.json ./
+
+RUN npm install
 
 # Install latest chrome dev package and fonts to support major charsets (Chinese, Japanese, Arabic, Hebrew, Thai and a few others)
 # Note: this installs the necessary libs to make the bundled version of Chromium that Puppeteer
@@ -30,19 +37,9 @@ RUN npm init -y &&  \
     # same layer as npm install to keep re-chowned files from using up several hundred MBs more space
     && groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
     && mkdir -p /home/pptruser/Downloads \
-    && chown -R pptruser:pptruser /home/pptruser \
-    && chown -R pptruser:pptruser /node_modules \
-    && chown -R pptruser:pptruser /package.json \
-    && chown -R pptruser:pptruser /package-lock.json
+    && chown -R pptruser:pptruser /home/pptruser
 
 RUN npm install -g typescript
-RUN npm link typescript
-
-WORKDIR /usr/src/app
-
-COPY package*.json ./
-
-RUN npm install
 
 COPY . .
 
--- src/controllers/PdfParserController.ts
+++ src/controllers/PdfParserController.ts
@@ -2,35 +2,50 @@ import {Request, Response} from "express";
 import puppeteer from "puppeteer/lib/cjs/puppeteer/node-puppeteer-core";
 
 export default class PdfParseController {
+
     static async parseUrl(request: Request, response: Response)
     {
-        const browser = await puppeteer.launch({
-            executablePath: 'google-chrome-stable',
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
-        const page = await browser.newPage();
+        try {
+            const browser = await puppeteer.launch({
+                executablePath: 'google-chrome-stable',
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+            const page = await browser.newPage();
 
-        await page.goto(request.body.url);
+            await page.goto(request.body.url);
 
-        const pdf = await page.pdf({format: 'a4' });
-        await browser.close();
+            const pdf = await page.pdf({format: 'a4' });
+            await browser.close();
 
-        return response.send(pdf);
+            return response.send(pdf);
+        } catch (error) {
+            return response.status(502).json({
+                "message": 'Failed to parse this view to PDF',
+                "error": error
+            });
+        }
     }
 
     static async parseHtml(request: Request, response: Response)
     {
-        const browser = await puppeteer.launch({
-            executablePath: 'google-chrome-stable',
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
-        const page = await browser.newPage();
-        await page.setContent(request.body.html)
+        try {
+            const browser = await puppeteer.launch({
+                executablePath: 'google-chrome-stable',
+                args: ['--no-sandbox', '--disable-setuid-sandbox']
+            });
+            const page = await browser.newPage();
+            await page.setContent(request.body.html)
 
-        const pdf = await page.pdf({ format: "a4" });
+            const pdf = await page.pdf({ format: "a4" });
 
-        await browser.close();
+            await browser.close();
 
-        return response.send(pdf);
+            return response.send(pdf);
+        } catch (error) {
+            return response.status(502).json({
+                "message": 'Failed to parse this HTML to PDF',
+                "error": error
+            });
+        }
     }
 }
