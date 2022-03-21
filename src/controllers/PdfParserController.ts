0d4723cef26a6c2efa8db66abd44cf177b442d59
Rafael Laurindo <rafaellaurindomf@icloud.com>
2022-03-21 14:36:56 -0300

    chore: add debug mode

--- src/controllers/PdfParserController.ts
+++ src/controllers/PdfParserController.ts
@@ -1,6 +1,8 @@
 import {Request, Response} from "express";
 import puppeteer from "puppeteer/lib/cjs/puppeteer/node-puppeteer-core";
 
+import debugIsActive from "../utils/debug-is-active";
+
 export default class PdfParseController {
 
     static async parseUrl(request: Request, response: Response)
@@ -15,6 +17,15 @@ export default class PdfParseController {
                 ],
             });
             const page = await browser.newPage();
+
+            if (debugIsActive()) {
+                page.on("console", (message) =>
+                    console.log(`${message.type().slice(0, 3).toUpperCase()}: ${message.text()}`)
+                )
+                    .on("pageerror", ({ message }) => console.log(message))
+                    .on("response", (response) => console.log(`${response.status()}: ${response.url()}`))
+                    .on("requestfailed", (request) => console.log(`${request.failure().errorText} ${request.url()}`));
+            }
 
             await page.goto(request.body.url);
 
@@ -42,7 +53,18 @@ export default class PdfParseController {
                     "--no-zygote"
                 ],
             });
+            
             const page = await browser.newPage();
+       
+            if (debugIsActive()) {
+                page.on("console", (message) =>
+                    console.log(`${message.type().slice(0, 3).toUpperCase()}: ${message.text()}`)
+                )
+                    .on("pageerror", ({ message }) => console.log(message))
+                    .on("response", (response) => console.log(`${response.status()}: ${response.url()}`))
+                    .on("requestfailed", (request) => console.log(`${request.failure().errorText} ${request.url()}`));
+            }
+
             await page.setContent(request.body.html)
 
             const pdf = await page.pdf({
